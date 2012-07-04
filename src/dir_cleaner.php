4bd4898c90d2f416f00e05aba11da37df6726c1d
Arnaud Tiérant <at@synap.fr>
2012-07-04 10:41:00 +0200

    Upd8 Script

--- src/dir_cleaner.php
+++ src/dir_cleaner.php
@@ -28,19 +28,20 @@ $folder2 = $argv[2];
 if ( !is_dir($folder1) && !is_dir($folder2) ) {
     echo "\nCould not open input folders : Folder 1 = $folder1 && Folder 2 = $folfder2\n\n";
     exit(1);
+}else{
+
+    $ezcFolder1 = ezcBaseFile::findRecursive(
+        $folder1,
+        array( "@./*.tpl$@" ),
+        array( '@/'.$folder2.'/@' ),
+        $stats
+    );
+    /*$ezcFolder2 = ezcBaseFile::findRecursive(
+        $folder2,
+        array( '.tpl$' )
+    );*/
+
+    var_dump( $ezcFolder1 );
+
+    echo ("There are ".$stats['count']." matching templates\n\n" );
 }
-
-$ezcFolder1 = ezcBaseFile::findRecursive(
-    $folder1,
-    array( "@./*.tpl$@" ),
-    array( '@/'.$folder2.'/@' ),
-    $stats
-);
-/*$ezcFolder2 = ezcBaseFile::findRecursive(
-    $folder2,
-    array( '.tpl$' )
-);*/
-
-var_dump( $ezcFolder1 );
-
-echo ("There are ".$stats['count']." matching templates\n\n" );
